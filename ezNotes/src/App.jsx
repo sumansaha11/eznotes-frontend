73483c5faba82d7ee756cb81686bd3d54b574559
Suman Saha <sumanrider7@gmail.com>
2024-08-26 15:50:31 +0530

    first commit

---
+++ ezNotes/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import SignUp from "./pages/SignUp/SignUp.jsx";
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/signup" exact element={<SignUp />} />
+    </Routes>
+  </Router>
+);
+
+const App = () => {
+  return <div>{routes}</div>;
+};
+
+export default App;
